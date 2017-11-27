6e85bf7200e23c5da639e2b0aa18090c0219ea93
Dario <dario.fuzinato@meetrics.com>
2017-11-27 15:54:18 +0100

    save test file

--- nodexonv.js
+++ nodexonv.js
@@ -1,9 +1,19 @@
 const TextToSVG = require('text-to-svg');
-const textToSVG = TextToSVG.loadSync();
+const fs = require('fs')
+// const textToSVG = TextToSVG.loadSync();
+const textToSVG = TextToSVG.loadSync('Pacifico.ttf');
  
-const attributes = {fill: 'red', stroke: 'black'};
+const attributes = {fill: 'red'};
 const options = {x: 0, y: 0, fontSize: 72, anchor: 'top', attributes: attributes};
  
 const svg = textToSVG.getSVG('hello', options);
- 
+
+fs.writeFile("test.svg", svg, 'utf8', function (err) {
+  if (err) {
+      return console.log(err);
+  }
+
+  console.log("The file was saved!");
+});
+
 console.log(svg);
